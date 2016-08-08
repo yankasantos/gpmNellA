45daf20256dc4b958ee7dd23880ab7fdbfec89d4
yanka.santos <yankasantos0107@gmail.com>
2016-08-08 17:47:02 -0300

    Larning Bootstrap

--- Allen-gpm-2016-07-12/Allen-gpm/js/gpm-action.js
+++ Allen-gpm-2016-07-12/Allen-gpm/js/gpm-action.js
@@ -1,16 +1,55 @@
-
+/***************Notificações*************************************************/
     $(document).ready(function(){
-        $(".icon-not").click(function(){
-            var toggle=$(".box-notificacoes").attr('data-toggle');
+
+         $(".icon-not").click(function(){
+            var toggle = $('.box-tudo-item').attr('data-toggle');
             if (toggle == 'true'){
-                $('.box-notificacoes')
+                $(this).parent().parent().find('.box-tudo-item')
                     .attr('data-toggle','false')
-                    .removeClass('icon-not');
+                    .removeClass('active');
             }
             else{
-                $('.box-notificacoes')
-                    .attr('data-toggle','true')
-                    .addClass('icon-not');
+                $(this).parent().parent().find('.box-tudo-item')
+                    .attr('data-toggle','true').addClass('active');
             }
         });
     });
+
+    /*$(this) .find(".box-tudo-item");
+      $('icon-not active') .parent() .parent() .find('box-tudo-item')
+    */
+
+   /* $(document).ready(function () {
+        $(".container > button").click(function () {
+            var toggle = $('.list-wrapper').attr('data-toggle');
+            if(toggle == 'true'){
+                $('.list-wrapper')
+                    .attr('data-toggle', 'false')
+                    .removeClass('active');
+
+            }
+            else{
+                $('.list-wrapper')
+                    .attr('data-toggle', 'true')
+                    .addClass('active');
+            }
+        });
+    });
+    *********************** Código de Caio *****************************
+    */
+/***************Notificações*************************************************/
+$(document).ready(function(){
+
+    $("drop-down-box").click(function(){
+        var toggle = $('.box-tudo-item').attr('data-toggle');
+        if (toggle == 'true'){
+            $(this).parent().parent().find('.box-tudo-item')
+                .attr('data-toggle','false')
+                .removeClass('active');
+        }
+        else{
+            $(this).parent().parent().find('.box-tudo-item')
+                .attr('data-toggle','true').addClass('active');
+        }
+    });
+});
