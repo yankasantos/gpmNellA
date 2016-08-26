20cfc8072d4718f090e03836537876bdbfcf3964
yanka.santos <yankasantos0107@gmail.com>
2016-08-26 17:57:24 -0300

    Learn Materialise

--- Allen-gpm-2016-07-12/Allen-gpm/gpm-materialize-app.js
+++ Allen-gpm-2016-07-12/Allen-gpm/gpm-materialize-app.js
@@ -10,3 +10,29 @@ $(document).ready(function(){
     $('.button-collapse').sideNav('hide');*/
 }) 
 /***********************BOTAO DE EDICAO*******************************/
+$(document).redy(function)
+    $("box-pencil").click(function(){
+
+    });
+
+
+//
+// $(document).ready(function(){
+//
+//     $(".icon-not").click(function(){
+//         var toggle = $('.box-tudo-item').attr('data-toggle');
+//         if (toggle == 'true'){
+//             $(this).parent().parent().find('.box-tudo-item')
+//                 .attr('data-toggle','false')
+//                 .removeClass('active');
+//         }
+//         else{
+//             $('.active')
+//                 .attr('data-toggle','false')
+//                 .removeClass('active');
+//             $(this).parent().parent().find('.box-tudo-item')
+//                 .attr('data-toggle','true').addClass('active');
+//
+//         }
+//     });
+// });
